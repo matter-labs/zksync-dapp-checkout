929d92c571234f6d85dee9c47e9765cdb7a40ab1
Jack Hamer <jackhamer0990@gmail.com>
2021-11-10 20:57:37 +0100

    feat: keep link data on /connect page

--- src/middleware/wallet.ts
+++ src/middleware/wallet.ts
@@ -1,10 +1,14 @@
 import { Context, Middleware } from "@nuxt/types";
+import useLinkHash from "@/plugins/useLinkHash";
 
 const walletMiddleware: Middleware = (context: Context) => {
   if (context.route.matched[0].path === "/link" || context.route.matched[0].path === "/link/:hash") {
     return;
   }
   if (context.store.getters["checkout/getErrorState"]) {
+    if(context.route.matched[0].path === "/connect" && typeof context.query.link === "string" && !context.store.getters["checkout/isLinkCheckout"]) {
+      return useLinkHash(context, context.query.link);
+    }
     context.redirect("/link");
     return;
   }
--- src/pages/link/_hash.vue
+++ src/pages/link/_hash.vue
@@ -4,45 +4,12 @@
 
 <script lang="ts">
 import Vue from "vue";
-
-import { parseDecimal } from "matter-dapp-module/utils";
-import { RestProvider } from "zksync";
-import { PaymentItem } from "@/types";
-import { decrypt } from "@/plugins/link";
+import useLinkHash from "@/plugins/useLinkHash";
 
 export default Vue.extend({
   layout: "link",
-  async fetch({store, params, redirect}) {
-    try {
-      store.commit("checkout/setLinkCheckoutState", true);
-      const syncProvider: RestProvider = await store.dispatch("zk-provider/requestProvider");
-      await store.dispatch("zk-tokens/loadZkTokens");
-      const transactions: PaymentItem[] = decrypt(params.hash);
-      store.commit("checkout/setError", false);
-      console.log("Transactions", transactions);
-      store.dispatch("checkout/setTransactionData", {
-        transactions: transactions.map((e, index) => ({
-          to: e.address,
-          token: e.token,
-          amount: parseDecimal(syncProvider, e.token, e.amount.toString()),
-          description: `Payment ${index+1}`,
-        })),
-        feeToken: "ETH",
-        fromAddress: undefined,
-      });
-      store.dispatch("checkout/requestUsedTokensPrice");
-      if(store.getters["zk-account/loggedIn"]) {
-        await Promise.all([
-          store.dispatch("checkout/requestInitialData"),
-          store.dispatch("zk-account/updateAccountState", true),
-        ]);
-      }
-      redirect("/connect");
-    } catch (error) {
-      console.log("zkLink error", error);
-      await store.dispatch("openModal", "zkLinkParseFail");
-      redirect("/link");
-    }
+  async fetch(ctx) {
+    await useLinkHash(ctx, ctx.params.hash);
   },
 });
 </script>
