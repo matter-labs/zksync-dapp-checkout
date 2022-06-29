5e16f8546a2c64d46036f763413971361861f6bc
Jack Hamer <47187316+JackHamer09@users.noreply.github.com>
2022-06-29 15:42:45 +0200

    fix: validate address, token and amount when using zklink (#107)
    * fix: validate address when using zklink
    
    * fix: validate token and amount from zklink

--- src/plugins/link.ts
+++ src/plugins/link.ts
@@ -1,4 +1,9 @@
-import { PaymentItem } from "@/types";
+import { isAddress } from "@ethersproject/address";
+import { Address, TokenSymbol } from "zksync/build/types";
+import { parseDecimal } from "@matterlabs/zksync-nuxt-core/utils";
+import { RestProvider } from "zksync";
+import { BigNumber } from "ethers";
+import { PaymentItem, ZkTokens } from "@/types";
 
 export const encrypt = (transactions: PaymentItem[]): string => {
   const hashedTransactions: string[] = [];
@@ -8,17 +13,31 @@ export const encrypt = (transactions: PaymentItem[]): string => {
   return encodeURI(window.btoa(hashedTransactions.join("#")).replace(/=/g, ""));
 };
 
-export const decrypt = (hash: string): PaymentItem[] => {
+type DecryptedPaymentItem = {
+  address: Address;
+  token: TokenSymbol;
+  amount: BigNumber;
+};
+
+export const decrypt = (hash: string, syncProvider: RestProvider, tokens: ZkTokens): DecryptedPaymentItem[] => {
   const decoded = window.atob(decodeURI(hash));
   const transactionHashes: string[] = decoded.split("#");
-  const transactions: PaymentItem[] = [];
+  const transactions: DecryptedPaymentItem[] = [];
   for (const item of transactionHashes) {
     const [address, token, amount] = item.split("|");
-    transactions.push({
-      address,
-      token,
-      amount,
-    });
+    if (!isAddress(address) || !Object.prototype.hasOwnProperty.call(tokens, token)) {
+      continue;
+    }
+    try {
+      transactions.push({
+        address,
+        token,
+        amount: parseDecimal(syncProvider, token, amount),
+      });
+    } catch (error) {
+      console.warn(`Failed to parse amount of ${amount} for token ${token}`);
+      continue;
+    }
   }
   return transactions;
 };
