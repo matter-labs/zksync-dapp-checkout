056c72329a5fa64faf0d65a69fe49c7167bc98f1
Jack Hamer <47187316+JackHamer09@users.noreply.github.com>
2023-08-14 20:17:39 +0200

    fix: update ramp sdk (#139)

--- src/components/BuyWithRampBtn.vue
+++ src/components/BuyWithRampBtn.vue
@@ -59,8 +59,8 @@ export default Vue.extend({
       })
         // Weird typing error
         // @ts-ignore
-        .on("PURCHASE_SUCCESSFUL", (event) => {
-          console.log("Ramp PURCHASE_SUCCESSFUL", event);
+        .on("PURCHASE_CREATED", (event) => {
+          console.log("Ramp PURCHASE_CREATED", event);
           this.$store.dispatch("checkout/requestUsedTokensEthereumBalance", true);
           this.$store.dispatch("zk-account/updateAccountState", true);
         })
